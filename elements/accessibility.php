960fad7cbe0109a1859246e407828cc5b45ca9bd
Kurt Menne <zero@zero-2.local>
2013-04-17 13:31:43 -0500

    added aria start

--- elements/accessibility.php
+++ elements/accessibility.php
@@ -5,15 +5,24 @@
         <h1>Accessibility</h1>  
     </div>
 
-    <p><a href="http://www.section508.gov/index.cfm?FuseAction=Content&ID=12#Web">Section 508 Standards for intranet and internet information and applications</a>.</p>
+    <p><a href="http://www.section508.gov/index.cfm?FuseAction=Content&ID=12#Web" target="_blank">Section 508 Standards for intranet and internet information and applications</a>.</p>
     
     <p class="indent">&mdash; Interfaces developed by Capella University should meet Section 508 standards.</p>
 
-    <p><a href="http://www.w3.org/TR/WCAG10/full-checklist.html">W3C checklist of checkpoints for accessibility</a>.</p>
+    <p><a href="http://www.w3.org/TR/WCAG10/full-checklist.html" target="_blank">W3C checklist of checkpoints for accessibility</a>.</p>
     
     <p class="indent">&mdash; Interfaces developed by Capella University <b>must</b> meet Priority 1 guidelines.</p>
     
-    <p class="indent">&mdash; <a href="http://www.w3.org/TR/1999/WAI-WEBCONTENT-19990505/#Guidelines">The WCAG 1.0 Guidelines</a>.</p>
+    <p class="indent">&mdash; <a href="http://www.w3.org/TR/1999/WAI-WEBCONTENT-19990505/#Guidelines" target="_blank">The WCAG 1.0 Guidelines</a>.</p>
+
+    <article id="aria">
+    	<h3>ARIA</h3>
+
+    	<p>As part of the Web Accessibility Initiative (WAI), the Accessible Rich Internet Applications Suite (ARIA), defines a way to make Web content and Web applications more accessible. It is used to improve the accessibility of dynamic content and advanced user interface controls developed with Ajax, HTML, JavaScript, and related technologies.</p>
+    
+    	<p><a href="http://www.w3.org/TR/wai-aria/roles" target="_blank">W3C ARIA Specification</a>.</p>
+    </article>       
+
 
 </section>
 
